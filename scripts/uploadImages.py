0b2b18767e5382278e485b573ca19bb4648053cd
David <davidsimonkastle@gmail.com>
2019-08-22 00:18:45 -0300

    update: post controller

--- scripts/uploadImages.py
+++ scripts/uploadImages.py
@@ -1,6 +1,6 @@
 import os
 import requests
-
+import json
 
 refTags = ['sala', 'mesas', 'gourmet', 'lavanderia', 'banheiro', 'cozinha', 'dormitorio',
            'varanda', 'studio', 'cervejaria', 'home', 'quarto', 'lavabo', 'closet', 'corporativo', 'solteiro', 'casal', 'unisex']
@@ -16,22 +16,22 @@ def corrigirDormitorio(e):
 def main():
 
     for filename in os.listdir("../../imagens_italinea/"):
-
-        # response = requests.post(
-       #     'http://18.191.111.54:1337/files-italinea', files={'file': open('filename', 'rb')})
-
-        # file_id = response.content.id
-
+        path = "../../imagens_italinea/" + filename
+        response = requests.post(
+            'http://18.191.111.54:1337/files-italinea', files={'file': open(path, 'rb')})
+        file_id = response.json()['id']
         tags = filename.split('-')
         tags = map(corrigirDormitorio, tags)
         tags = list(
             filter(lambda x: (len(x) > 3) and (not '.jpg' in x) and (x in refTags), tags))
 
-        print(tags)
-        # dst = filename.split(' ')[0] + '.jpg'
-        # src = "../../imagens_italinea/" + filename
-        # dst = "../../imagens_italinea/" + dst
-        # os.rename(src, dst)
+        payload = {'tags': tags,
+                   'title': 'Imagem da Italinea Torre', 'image_id': file_id}
+        print(payload)
+        response = requests.post(
+            'http://18.191.111.54:1337/posts', data=payload)
+
+        print(response)
 
 
 # Driver Code
