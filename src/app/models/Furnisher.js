b49aa069f20546ff90da3e732bd53613ecb86f0d
David <davidsimonkastle@gmail.com>
2019-08-22 21:32:59 -0300

    update: added furnisher Post model

---
+++ src/app/models/Furnisher.js
@@ -0,0 +1,27 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Furnisher extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        description: Sequelize.TEXT,
+        email: Sequelize.STRING,
+        contact: Sequelize.STRING,
+        address: Sequelize.STRING,
+        social: Sequelize.JSON,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'furnisher' });
+  }
+}
+
+export default Furnisher;
