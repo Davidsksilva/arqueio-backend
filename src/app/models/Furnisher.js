4388611a1295bcd82b9d486106c742dbdeb996dc
David <davidsimonkastle@gmail.com>
2019-08-24 10:42:16 -0300

    update: fix funisher, post

--- src/app/models/Furnisher.js
+++ src/app/models/Furnisher.js
@@ -20,7 +20,7 @@ class Furnisher extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.File, { foreignKey: 'image', as: 'image' });
+    this.belongsTo(models.File, { foreignKey: 'image', as: 'imageUrl' });
   }
 }
 
