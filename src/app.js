d6a14b07be4a9e860f50f65dfc8a6f4749d4a0a1
David <davidsimonkastle@gmail.com>
2019-09-05 19:59:05 -0300

    fix: removed unused code

--- src/app.js
+++ src/app.js
@@ -24,41 +24,6 @@ class App {
     );
   }
 
-  socketIO() {
-    this.io = socketIO(this.server);
-    this.mobileSockets = {};
-
-    this.io.on('connection', socket => {
-      socket.on('newUser', async payload => {
-        const { id } = payload;
-
-        const user = await User.findByPk(id);
-        const users = await User.findAll();
-
-        this.mobileSockets[user.id] = socket.id;
-        socket.emit('userCreated', { user, users });
-        socket.broadcast.emit('newUser', user);
-      });
-
-      socket.on('chat', async users => {
-        const conversation = ConversationController.findOrCreateConversation(
-          users.user.id,
-          users.receiver.id
-        );
-
-        socket.emit('priorMessages', conversation.messages);
-      });
-
-      socket.on('message', async ({ text, sender, receiver }) => {
-        const message = MessageController.createMessage(text, sender, receiver);
-
-        socket.emit('incomingMessage', message);
-        const receiverSocketId = this.mobileSockets[receiver.id];
-        socket.to(receiverSocketId).emit('incomingMessage', message);
-      });
-    });
-  }
-
   routes() {
     this.server.use(routes);
   }
